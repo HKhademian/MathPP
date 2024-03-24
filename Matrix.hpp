a14a503a25f4995b7a2c5c12ff0d2deb2ecfae72
Hossain Khademian <hossain.khademian@gmail.com>
2024-03-24 21:05:53 +0100

    scalar ops

--- Matrix.hpp
+++ Matrix.hpp
@@ -36,11 +36,130 @@ namespace MathPP
         constexpr static const auto COL_SKIP = COL_SIZE + COL_SPACE_SIZE;
         constexpr static const auto ROW_SKIP = ROW_SIZE + ROW_SPACE_SIZE;
 
-        _DATA_TYPE elements[ROW_COUNT * ROW_SIZE +
-                            ROW_SPACE_COUNT * ROW_SPACE_SIZE] = {0};
+        TYPE elements[ROW_COUNT * ROW_SIZE +
+                      ROW_SPACE_COUNT * ROW_SPACE_SIZE] = {0};
 
-        _DATA_TYPE const &at(size_t row, size_t col) const { return elements[row * ROW_SKIP + col * COL_SKIP]; }
-        _DATA_TYPE &at(size_t row, size_t col) { return elements[row * ROW_SKIP + col * COL_SKIP]; }
+        TYPE const &at(size_t row, size_t col) const { return elements[row * ROW_SKIP + col * COL_SKIP]; }
+        TYPE &at(size_t row, size_t col) { return elements[row * ROW_SKIP + col * COL_SKIP]; }
+
+        TYPE const &get(size_t row, size_t col, TYPE const &def = 0) const
+        {
+            return row < ROW_COUNT && col < COL_COUNT ? at(row, col) : def;
+        }
+        TYPE &get(size_t row, size_t col, TYPE &def = 0)
+        {
+            return row < ROW_COUNT && col < COL_COUNT ? at(row, col) : def;
+        }
     };
+
+    template <
+        size_t _COLS1, size_t _ROWS1, typename _DATA_TYPE1, size_t _COL_SPACE1, size_t _ROW_SPACE1,
+        size_t _COLS2, size_t _ROWS2, typename _DATA_TYPE2, size_t _COL_SPACE2, size_t _ROW_SPACE2>
+    auto operator+(
+        Matrix<_COLS1, _ROWS1, _DATA_TYPE1, _COL_SPACE1, _ROW_SPACE1> const &lhs,
+        Matrix<_COLS2, _ROWS2, _DATA_TYPE2, _COL_SPACE2, _ROW_SPACE2> const &rhs)
+    {
+        constexpr const auto COL_COUNT = _COLS1 > _COLS2 ? _COLS1 : _COLS2;
+        constexpr const auto ROW_COUNT = _ROWS1 > _ROWS2 ? _ROWS1 : _ROWS2;
+        typedef typename MathOp<_DATA_TYPE1, _DATA_TYPE2>::plus DATA_TYPE;
+
+        Matrix<COL_COUNT, ROW_COUNT, DATA_TYPE, 0, 0> ret;
+        for (auto r = 0; r < ret.ROW_COUNT; ++r)
+        {
+            for (auto c = 0; c < ret.COL_COUNT; ++c)
+            {
+                ret.at(r, c) = lhs.get(r, c) + rhs.get(r, c);
+            }
+        }
+        return ret;
+    }
+
+    template <
+        size_t _COLS1, size_t _ROWS1, typename _DATA_TYPE1, size_t _COL_SPACE1, size_t _ROW_SPACE1,
+        size_t _COLS2, size_t _ROWS2, typename _DATA_TYPE2, size_t _COL_SPACE2, size_t _ROW_SPACE2>
+    auto operator-(
+        Matrix<_COLS1, _ROWS1, _DATA_TYPE1, _COL_SPACE1, _ROW_SPACE1> const &lhs,
+        Matrix<_COLS2, _ROWS2, _DATA_TYPE2, _COL_SPACE2, _ROW_SPACE2> const &rhs)
+    {
+        constexpr const auto COL_COUNT = _COLS1 > _COLS2 ? _COLS1 : _COLS2;
+        constexpr const auto ROW_COUNT = _ROWS1 > _ROWS2 ? _ROWS1 : _ROWS2;
+        typedef typename MathOp<_DATA_TYPE1, _DATA_TYPE2>::minus DATA_TYPE;
+
+        Matrix<COL_COUNT, ROW_COUNT, DATA_TYPE, 0, 0> ret;
+        for (auto r = 0; r < ret.ROW_COUNT; ++r)
+        {
+            for (auto c = 0; c < ret.COL_COUNT; ++c)
+            {
+                ret.at(r, c) = lhs.get(r, c) - rhs.get(r, c);
+            }
+        }
+        return ret;
+    }
+
+    template <
+        typename _DATA_TYPE1,
+        size_t _COLS2, size_t _ROWS2, typename _DATA_TYPE2, size_t _COL_SPACE2, size_t _ROW_SPACE2>
+    auto operator*(
+        _DATA_TYPE1 const &lhs,
+        Matrix<_COLS2, _ROWS2, _DATA_TYPE2, _COL_SPACE2, _ROW_SPACE2> const &rhs)
+    {
+        constexpr const auto COL_COUNT = _COLS2;
+        constexpr const auto ROW_COUNT = _ROWS2;
+        typedef typename MathOp<_DATA_TYPE1, _DATA_TYPE2>::multiply DATA_TYPE;
+
+        Matrix<COL_COUNT, ROW_COUNT, DATA_TYPE, 0, 0> ret;
+        for (auto r = 0; r < ret.ROW_COUNT; ++r)
+        {
+            for (auto c = 0; c < ret.COL_COUNT; ++c)
+            {
+                ret.at(r, c) = lhs * rhs.get(r, c);
+            }
+        }
+        return ret;
+    }
+
+    template <
+        size_t _COLS1, size_t _ROWS1, typename _DATA_TYPE1, size_t _COL_SPACE1, size_t _ROW_SPACE1,
+        typename _DATA_TYPE2>
+    auto operator*(
+        Matrix<_COLS1, _ROWS1, _DATA_TYPE1, _COL_SPACE1, _ROW_SPACE1> const &lhs,
+        _DATA_TYPE2 const &rhs)
+    {
+        constexpr const auto COL_COUNT = _COLS1;
+        constexpr const auto ROW_COUNT = _ROWS1;
+        typedef typename MathOp<_DATA_TYPE1, _DATA_TYPE2>::multiply DATA_TYPE;
+
+        Matrix<COL_COUNT, ROW_COUNT, DATA_TYPE, 0, 0> ret;
+        for (auto r = 0; r < ret.ROW_COUNT; ++r)
+        {
+            for (auto c = 0; c < ret.COL_COUNT; ++c)
+            {
+                ret.at(r, c) = lhs.get(r, c) * rhs;
+            }
+        }
+        return ret;
+    }
+
+    template <
+        size_t _COLS1, size_t _ROWS1, typename _DATA_TYPE1, size_t _COL_SPACE1, size_t _ROW_SPACE1,
+        typename _DATA_TYPE2>
+    auto operator/(
+        Matrix<_COLS1, _ROWS1, _DATA_TYPE1, _COL_SPACE1, _ROW_SPACE1> const &lhs,
+        _DATA_TYPE2 const &rhs)
+    {
+        constexpr const auto COL_COUNT = _COLS1;
+        constexpr const auto ROW_COUNT = _ROWS1;
+        typedef typename MathOp<_DATA_TYPE1, _DATA_TYPE2>::division DATA_TYPE;
+
+        Matrix<COL_COUNT, ROW_COUNT, DATA_TYPE, 0, 0> ret;
+        for (auto r = 0; r < ret.ROW_COUNT; ++r)
+        {
+            for (auto c = 0; c < ret.COL_COUNT; ++c)
+            {
+                ret.at(r, c) = lhs.get(r, c) / rhs;
+            }
+        }
+        return ret;
+    }
 
 }
--- Matrix.test.cpp
+++ Matrix.test.cpp
@@ -6,11 +6,25 @@ using namespace MathPP;
 
 int main()
 {
+    Matrix<3, 4, int> mat1{101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112};
+    Matrix<4, 3, float> mat2{201.1, 202.1, 203.1, 204.1, 205.1, 206.1, 207.1, 208.1, 209.1, 210.1, 211.1, 212.1};
+
     /*test1*/ {
-        Matrix<3, 4, float> mat{101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112};
-        std::cout << "float size:" << sizeof(float) << std::endl;
-        std::cout << "mat float 3x4 size:" << sizeof(mat) << std::endl;
-        std::cout << "mat float 3x4:" << mat << std::endl;
+        std::cout << "float size: " << sizeof(float) << std::endl;
+        std::cout << "mat1 float 3x4 size: " << sizeof(mat1) << std::endl;
+        std::cout << std::endl;
+    }
+
+    /*test2*/ {
+        std::cout << "mat1 float 3x4: " << mat1 << std::endl;
+        std::cout << "mat2 float 4x3: " << mat2 << std::endl;
+        std::cout << std::endl;
+    }
+
+    /*test3*/ {
+        auto mat = mat1 + mat2;
+        std::cout << "mat1+mat2 : cols= " << mat.COL_COUNT << std::endl;
+        std::cout << "mat1+mat2 : rows= " << mat.ROW_COUNT << std::endl;
         for (auto r = 0; r < mat.ROW_COUNT; ++r)
         {
             for (auto c = 0; c < mat.COL_COUNT; ++c)
@@ -19,6 +33,56 @@ int main()
             }
             std::cout << std::endl;
         }
+        std::cout << "mat1+mat2 : " << mat << std::endl;
+        std::cout << std::endl;
+    }
+
+    /*test4*/ {
+        auto mat = mat2 - mat1;
+        std::cout << "mat2-mat1 : cols= " << mat.COL_COUNT << std::endl;
+        std::cout << "mat2-mat1 : rows= " << mat.ROW_COUNT << std::endl;
+        for (auto r = 0; r < mat.ROW_COUNT; ++r)
+        {
+            for (auto c = 0; c < mat.COL_COUNT; ++c)
+            {
+                std::cout << mat.at(r, c) << '\t';
+            }
+            std::cout << std::endl;
+        }
+        std::cout << "mat2-mat1 : " << mat << std::endl;
+        std::cout << std::endl;
+    }
+
+    /*test5*/ {
+        auto mat = 3 * mat1;
+        std::cout << "3 * mat1 : cols= " << mat.COL_COUNT << std::endl;
+        std::cout << "3 * mat1 : rows= " << mat.ROW_COUNT << std::endl;
+        std::cout << "3 * mat1 : " << mat << std::endl;
+        std::cout << std::endl;
+    }
+
+    /*test6*/ {
+        auto mat = mat1 * 2.5;
+        std::cout << "mat1 * 2.5 : cols= " << mat.COL_COUNT << std::endl;
+        std::cout << "mat1 * 2.5 : rows= " << mat.ROW_COUNT << std::endl;
+        std::cout << "mat1 * 2.5 : " << mat << std::endl;
+        std::cout << std::endl;
+    }
+
+    /*test7*/ {
+        auto mat = mat1 / 7;
+        std::cout << "mat1 / 7 : cols= " << mat.COL_COUNT << std::endl;
+        std::cout << "mat1 / 7 : rows= " << mat.ROW_COUNT << std::endl;
+        std::cout << "mat1 / 7 : " << mat << std::endl;
+        std::cout << std::endl;
+    }
+
+    /*test8*/ {
+        auto mat = mat1 / 1.1;
+        std::cout << "mat1 / 1.1 : cols= " << mat.COL_COUNT << std::endl;
+        std::cout << "mat1 / 1.1 : rows= " << mat.ROW_COUNT << std::endl;
+        std::cout << "mat1 / 1.1 : " << mat << std::endl;
+        std::cout << std::endl;
     }
 
     return 0;
