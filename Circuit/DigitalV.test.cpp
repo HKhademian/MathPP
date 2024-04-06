9ecc09e3ed9ce7d53d5a978d3c59f720cfe65a6c
Hossain Khademian <hossain.khademian@gmail.com>
2024-04-06 18:20:51 +0200

    DigitalV:: complete test

---
+++ Circuit/DigitalV.test.cpp
@@ -0,0 +1,52 @@
+/// clear && gcc -o ./Circuit/DigitalV.out ./Circuit/DigitalV.test.cpp -lstdc++ && echo "9 8 7 6 5 4 3 2 1" | ./Circuit/DigitalV.out | tee ./Circuit/DigitalV.test.out && rm ./Circuit/DigitalV.out
+
+#include "DigitalV.hpp"
+#include "DigitalV.iostream.hpp"
+#include <iostream>
+
+using namespace CircuitPP;
+
+const auto V0 = DigitalV::V0;
+const auto V1 = DigitalV::V1;
+
+int main(void)
+{
+    std::cout << "START" << std::endl;
+
+    std::cout << "V0: " << DigitalV::V0 << std::endl;
+    std::cout << "V1: " << DigitalV::V1 << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "! V0: " << !V0 << std::endl;
+    std::cout << "! V1: " << !V1 << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "V0 & V0: " << (V0 & V0) << std::endl;
+    std::cout << "V0 & V1: " << (V0 & V1) << std::endl;
+    std::cout << "V1 & V1: " << (V1 & V1) << std::endl;
+    std::cout << "V0 & V1: " << (V0 & V1) << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "V0 | V0: " << (V0 | V0) << std::endl;
+    std::cout << "V0 | V1: " << (V0 | V1) << std::endl;
+    std::cout << "V1 | V1: " << (V1 | V1) << std::endl;
+    std::cout << "V0 | V1: " << (V0 | V1) << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "V0 ^ V0: " << (V0 ^ V0) << std::endl;
+    std::cout << "V0 ^ V1: " << (V0 ^ V1) << std::endl;
+    std::cout << "V1 ^ V1: " << (V1 ^ V1) << std::endl;
+    std::cout << "V0 ^ V1: " << (V0 ^ V1) << std::endl;
+    std::cout << std::endl;
+
+    {
+        std::cout << "Complex1: (!(!(!(V0 & V1)))) === " << (!(!(!(V0 & V1)))) << std::endl;
+        std::cout << "Complex2: (!V0 ^ V1) === " << (!V0 ^ V1) << std::endl;
+        std::cout << "Complex3: (V1 & !V0 ^ V1) === " << (V1 & !V0 ^ V1) << std::endl;
+        std::cout << "Complex4: (!(V1 & !V0 ^ V1)) === " << (!(V1 & !V0 ^ V1)) << std::endl;
+        std::cout << std::endl;
+    }
+
+    std::cout << "FINISH" << std::endl;
+    return 0;
+}
--- tests.sh
+++ tests.sh
@@ -18,5 +18,8 @@ echo "4 3 2 1 0 -1 -2 -3 -4" | ./Math/Matrix.test.o | tee ./Math/Matrix.test.out
 $CC ./Math/MatrixTrilean.test.cpp -lstdc++ -o ./Math/MatrixTrilean.test.o
 echo "4 3 2 1 0 -1 -2 -3 -4" | ./Math/MatrixTrilean.test.o | tee ./Math/MatrixTrilean.test.out && rm ./Math/MatrixTrilean.test.o
 
+$CC ./Circuit/DigitalV.test.cpp -lstdc++ -o ./Circuit/DigitalV.test.o
+echo "4 3 2 1 0 -1 -2 -3 -4" | ./Circuit/DigitalV.test.o | tee ./Circuit/DigitalV.test.out && rm ./Circuit/DigitalV.test.o
+
 $CC ./Circuit/Logic.test.cpp -lstdc++ -o ./Circuit/Logic.test.o
 echo "4 3 2 1 0 -1 -2 -3 -4" | ./Circuit/Logic.test.o | tee ./Circuit/Logic.test.out && rm ./Circuit/Logic.test.o
